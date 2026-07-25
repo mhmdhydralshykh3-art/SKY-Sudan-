5baac25771cc9c03a027925d81ea0193735dc59d
mhmdhydralshykh3-art <mhmdhydralshykh3@gmail.com>
2026-07-26 00:15:06 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.skysudan.app",
+  appName: "Sky Sudan",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
